8bcdf8a96d87f4a8969be0e38032e13042daee2e
Michael Antoš <128068126+antosmichael07@users.noreply.github.com>
2024-06-05 09:02:46 +0200

    updated to the v1.7.0 go tcp connection package

--- client/main.go
+++ client/main.go
@@ -1,21 +1,25 @@
 package main
 
 import (
-	custom_logger "github.com/antosmichael07/Go-Logger"
+	lgr "github.com/antosmichael07/Go-Logger"
 	tcp "github.com/antosmichael07/Go-TCP-Connection"
 )
 
-var logger = custom_logger.NewLogger()
+var logger = lgr.NewLogger("Tutak")
 
 func main() {
 	client := tcp.NewClient("localhost:8080")
 	client.Connect()
 
 	client.On("test", func(data string) {
-		logger.Log("Data received: %s", data)
+		logger.Log(lgr.Info, "wow")
 	})
 
-	client.SendData("test", "Hello from client")
+	client.On("test2", func(data string) {
+		logger.Log(lgr.Info, "bomba")
+	})
+
+	client.SendData("execute", "Hello from client")
 
 	client.Listen()
 }
--- server/main.go
+++ server/main.go
@@ -1,16 +1,22 @@
 package main
 
 import (
-	custom_logger "github.com/antosmichael07/Go-Logger"
+	"time"
+
+	lgr "github.com/antosmichael07/Go-Logger"
 	tcp "github.com/antosmichael07/Go-TCP-Connection"
 )
 
-var logger = custom_logger.NewLogger()
+var logger = lgr.NewLogger("Tutak")
 
 func main() {
 	server := tcp.NewServer("localhost:8080")
 
-	server.SendData("test", "Hello from server")
+	server.On("execute", func(data string) {
+		server.SendData("test", "Hello from server")
+		time.Sleep(3*time.Second)
+		server.SendData("test2", "Hello from server")
+	})
 
 	server.Start()
 }
